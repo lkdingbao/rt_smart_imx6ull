2530f668f2f47c188b57bb78f710ba2e50f1438f
dengchow <ytesliang@163.com>
2021-03-13 18:10:36 +0800

    2021/03/13-18:04 PC-Name:HOME-DENGCHOW Edit: 1. 修改脚本文件：smart-env.bat -> autoconfig.bat，以兼容其他芯片

---
+++ rt-smart/kernel/bsp/imx6ull/envconfig.bat
@@ -0,0 +1,9 @@
+@set RTT_ROOT=E:\0.SourceCode\rt-smart\rt_smart_imx6ull\rt-smart\kernel
+
+@set RTT_CC=gcc
+@set RTT_CC_PREFIX=arm-linux-musleabi-
+
+@set RTT_EXEC_PATH=%ENV_ROOT%\tools\gnu_gcc\arm_gcc\musleabi\bin
+@set PATH=%RTT_EXEC_PATH%;%ENV_ROOT%\tools\gnu_gcc\arm_gcc\mingw\bin;%PATH%
+
+@echo config finished.
